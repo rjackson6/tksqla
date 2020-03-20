e584651aa2d22bcaaee6584f36e52bda470fbbd1
Ryan Jackson <supra621@gmail.com>
2020-03-20 01:28:38 -0400

    Preference menu selection events, sub frame stacking for different preference groups.

--- tksqla/menus.py
+++ tksqla/menus.py
@@ -29,15 +29,26 @@ class Preferences(tk.Frame):
         right_frame.grid(row=0, column=1, sticky='NSEW')
         left_frame.columnconfigure(0, weight=1)
         # placeholder label
-        pref_tree = ttk.Treeview(left_frame)
-        pref_tree.insert('', 'end', iid='appearance', text='Appearance!')
-        pref_tree.insert('', 'end', iid='general', text='General Settings!')
-        pref_tree.grid(row=0, column=0)
+        self.pref_tree = ttk.Treeview(left_frame)
+        self.pref_tree.bind('<<TreeviewSelect>>', self.treeview_select)
+        self.pref_tree.insert('', 'end', iid='appearance', text='Appearance!')
+        self.pref_tree.insert('', 'end', iid='general', text='General Settings!')
+        self.pref_tree.grid(row=0, column=0)
 
+        right_frame.rowconfigure(0, weight=1)
+        right_frame.columnconfigure(0, weight=1)
         self.appearance_frame = PreferencesAppearance(right_frame, self.callbacks)
         self.general_frame = PreferencesGeneral(right_frame, self.callbacks)
-        self.appearance_frame.grid(row=0, column=0)
-        self.general_frame.grid(row=0, column=0)
+        self.appearance_frame.grid(row=0, column=0, sticky='NSEW')
+        self.general_frame.grid(row=0, column=0, sticky='NSEW')
+
+    def treeview_select(self, event):
+        selection = self.pref_tree.selection()
+        iid = selection[0]  # first value in the tuple
+        if iid == 'appearance':
+            self.appearance_frame.lift()
+        elif iid == 'general':
+            self.general_frame.lift()
 
 
 class PreferencesAppearance(tk.Frame):
